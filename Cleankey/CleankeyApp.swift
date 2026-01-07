a168d79e25b146bbb46de76de465d12e6bdc6138
Nick <seafyre@icloud.com>
2026-01-07 18:00:39 +0100

    Initial Commit

---
+++ Cleankey/CleankeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleankeyApp.swift
+//  Cleankey
+//
+//  Created by Nick Ringelmann on 07.01.26.
+//
+
+import SwiftUI
+
+@main
+struct CleankeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
